00e0ec2f083812ac7bfe6fba2ce44e17ef152637
Maksaks <maksym.zhmutskyi@gmail.com>
2024-02-03 19:45:02 +0200

    Creating consumption of ingredients page

--- src/pages/Admin/Statistics/Consumption.tsx
+++ src/pages/Admin/Statistics/Consumption.tsx
@@ -1,6 +1,12 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { motion } from 'framer-motion'
 import { FC, useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
+import { toast } from 'react-toastify'
+import { isoStringToDatetimeLocal } from '../../../helper/date-formater.helper'
+import { StatisticsService } from '../../../services/Admin/StatisticsServices'
+import { IConsumptionIngredientsData } from '../../../types/IConsumptionIngredientsData'
 import {
 	IPointNameAndID,
 	IPointNameAndIDLoader,
@@ -10,14 +16,84 @@ const Consumption: FC = () => {
 	const { points } = useLoaderData() as IPointNameAndIDLoader
 	const [searchedPoints, setSearchedPoints] = useState<IPointNameAndID[]>()
 	const [selectedPoint, setSelectedPoint] = useState<IPointNameAndID>()
+	const [consumptionIngredients, setConsumptionIngredients] = useState<
+		IConsumptionIngredientsData[]
+	>([])
 
+	const [fromDate, setFromDate] = useState<string>()
+	const [toDate, setToDate] = useState<string>()
 	useEffect(() => {
 		if (points) {
 			setSearchedPoints(points)
+			const currentDate = new Date()
+			const lastMonthDate = new Date()
+			lastMonthDate.setMonth(currentDate.getMonth() - 1)
+			setFromDate(isoStringToDatetimeLocal(lastMonthDate.toString()))
+			setToDate(isoStringToDatetimeLocal(currentDate.toString()))
 		}
 	}, [])
 
-	useEffect(() => {}, [selectedPoint])
+	useEffect(() => {
+		if (
+			!fromDate ||
+			!fromDate.length ||
+			!toDate ||
+			!toDate.length ||
+			!selectedPoint
+		)
+			return
+		const getInfo = async () => {
+			try {
+				const data = await StatisticsService.getConsumptionOfIngredientsByPoint(
+					selectedPoint.id,
+					{
+						from: new Date(fromDate).toISOString(),
+						to: new Date(toDate).toISOString(),
+					}
+				)
+				if (data) {
+					setConsumptionIngredients(
+						data.sort((a, b) => b.consumption - a.consumption)
+					)
+				}
+			} catch (err: any) {
+				const error = err.response?.data.message
+				toast.error(error.toString())
+			}
+		}
+		getInfo()
+	}, [selectedPoint])
+
+	useEffect(() => {
+		if (
+			!fromDate ||
+			!fromDate.length ||
+			!toDate ||
+			!toDate.length ||
+			!selectedPoint
+		)
+			return
+		const getInfo = async () => {
+			try {
+				const data = await StatisticsService.getConsumptionOfIngredientsByPoint(
+					selectedPoint.id,
+					{
+						from: new Date(fromDate).toISOString(),
+						to: new Date(toDate).toISOString(),
+					}
+				)
+				if (data) {
+					setConsumptionIngredients(
+						data.sort((a, b) => b.consumption - a.consumption)
+					)
+				}
+			} catch (err: any) {
+				const error = err.response?.data.message
+				toast.error(error.toString())
+			}
+		}
+		getInfo()
+	}, [fromDate, toDate])
 
 	const searchHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
 		if (!e.target.value.length) {
@@ -71,8 +147,65 @@ const Consumption: FC = () => {
 			</div>
 			<div className='h-[100%] w-[65%] bg-zinc-600 rounded-3xl flex items-center flex-col relative'>
 				<h2 className='w-full h-[6%] text-center text-2xl uppercase font-bold border-b-4 p-3'>
-					POINT`S MENU POSITIONS POPULARITY
+					CONSUMPTION OF INGREDIENTS
 				</h2>
+				{selectedPoint && (
+					<div className='grid grid-cols-12 gap-2 p-3 w-full border-b-4'>
+						<small className='uppercase text-lg text-right font-bold my-auto'>
+							from:
+						</small>
+						<input
+							className='col-span-3 p-1 rounded-xl text-black'
+							type='datetime-local'
+							value={fromDate}
+							onChange={e => setFromDate(e.target.value)}
+						/>
+						<small className='uppercase text-lg text-right font-bold my-auto'>
+							to:
+						</small>
+						<input
+							className='col-span-3 p-1 rounded-xl text-black'
+							type='datetime-local'
+							value={toDate}
+							onChange={e => setToDate(e.target.value)}
+						/>
+					</div>
+				)}
+				{consumptionIngredients?.length ? (
+					<div className='w-[90%] bg-zinc-700/95 h-[70px] rounded-t-2xl grid grid-cols-10 items-center px-5 py-2 font-bold text-xl'>
+						<label>ID</label>
+						<label className='col-span-4 text-center'>NAME</label>
+						<label className='col-span-3 text-center'>CONSUMPTION</label>
+						<label className='col-span-2 text-center'>PRICE</label>
+					</div>
+				) : (
+					<></>
+				)}
+				<div className='w-full h-[80%] flex flex-col items-center gap-3 py-3 overflow-auto'>
+					{consumptionIngredients?.length ? (
+						consumptionIngredients.map((item, indx) => {
+							return (
+								<div
+									key={indx}
+									className='w-[90%] bg-zinc-400 h-[60px] rounded-2xl grid grid-cols-10 items-center px-5 py-2 text-lg text-black/80'
+								>
+									<label>{item.id}</label>
+									<label className='col-span-4 text-center'>{item.name}</label>
+									<label className='col-span-3 text-center'>
+										{item.consumption}
+									</label>
+									<label className='col-span-2 text-center'>
+										{item.price.toFixed(2)} UAH
+									</label>
+								</div>
+							)
+						})
+					) : (
+						<h2 className='uppercase text-white/70 font-bold text-xl w-full text-center'>
+							No data
+						</h2>
+					)}
+				</div>
 			</div>
 		</motion.div>
 	)
--- src/pages/Admin/Statistics/Popularity.tsx
+++ src/pages/Admin/Statistics/Popularity.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+/* eslint-disable react-hooks/exhaustive-deps */
 import { motion } from 'framer-motion'
 import { FC, useEffect, useState } from 'react'
 import { useLoaderData } from 'react-router-dom'
